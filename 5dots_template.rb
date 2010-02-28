dcf46faf4fc78aa339fbd42df6b957f97b73a81f
Mario <marioosh@5dots.pl>
2010-03-01 00:07:57 +0100

    Added RSpec and Cucumber Support

--- 5dots_template.rb
+++ 5dots_template.rb
@@ -1,4 +1,13 @@
 #5dots rails template
+#What do you need to succes run this template:
+# 1. Installed Git SCM
+# Installed gems:
+# - Rspec
+# - Rspec-rails
+# - Cucumber
+# - Cucumber-rails
+# - Factory girl
+# - Webrat
 
 #initialize git repository
 git :init
@@ -25,3 +34,29 @@ git :add  => "."
 #first commit to save changes
 
 git :commit   => "-a -m 'Initial commit' "
+
+#installing rspec, rpspec-rails, cucumber and factory_girl as a testing platform,
+#
+gem 'factory_girl'
+gem 'webrat'
+
+rake "gems:install"
+
+#removing standard 'test' folder.
+run 'rm -rf test'
+
+
+#generates RSpec and Cucumber generators and tasks
+generate :rspec
+generate :cucumber
+
+#making folder for factories
+run 'mkdir spec/factories'
+
+#comitting Test Facility to repository
+git :add  => '.'
+git :commit  => "-a -m 'Building Testing Facility' "
+
+
+
+
